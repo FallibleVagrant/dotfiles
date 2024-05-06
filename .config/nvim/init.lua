3f9bb4c0bab2ace34472473bc3b73cd24273f5a2
FallibleVagrant <124470389+FallibleVagrant@users.noreply.github.com>
2024-05-06 13:16:34 -0700

    Initial commit.

---
+++ .config/nvim/init.lua
@@ -0,0 +1,92 @@
+--[[
+
+This configuration file uses Kickstart as a base.
+It is recommended that you use the Kickstart repo to build your own configuration,
+and to avoid using this file except as reference or inspiration.
+
+Important comments from the original have been deleted,
+because I personally have no more use for them.
+
+-FV
+
+https://github.com/nvim-lua/kickstart.nvim
+
+=====================================================================
+=====================================================================
+========                                    .-----.          ========
+========         .----------------------.   | === |          ========
+========         |.-""""""""""""""""""-.|   |-----|          ========
+========         ||                    ||   | === |          ========
+========         ||    HELLO, WORLD    ||   |-----|          ========
+========         ||                    ||   | === |          ========
+========         ||                    ||   |-----|          ========
+========         ||:Tutor              ||   |:::::|          ========
+========         |'-..................-'|   |____o|          ========
+========         `"")----------------(""`   ___________      ========
+========        /::::::::::|  |::::::::::\  \ no mouse \     ========
+========       /:::========|  |==hjkl==:::\  \ required \    ========
+========      '""""""""""""'  '""""""""""""'  '""""""""""'   ========
+========                                                     ========
+=====================================================================
+=====================================================================
+
+  Kickstart.nvim is a starting point for your own configuration.
+    The goal is that you can read every line of code, top-to-bottom, understand
+    what your configuration is doing, and modify it to suit your needs.
+
+    Once you've done that, you can start exploring, configuring and tinkering to
+    make Neovim your own! That might mean leaving Kickstart just the way it is for a while
+    or immediately breaking it into modular pieces. It's up to you!
+
+    If you don't know anything about Lua:
+    https://learnxinyminutes.com/docs/lua/
+
+    Use `:help lua-guide` as a
+    reference for how Neovim integrates Lua.
+    - :help lua-guide
+    - (or HTML version): https://neovim.io/doc/user/lua-guide.html
+
+  Run AND READ `:help`.
+    This will open up a help window with some basic information
+    about reading, navigating and searching the builtin help documentation.
+
+    This should be the first place you go to look when you're stuck or confused
+    with something. It's one of my favorite Neovim features.
+
+    MOST IMPORTANTLY, we provide a keymap "<space>sh" to [s]earch the [h]elp documentation,
+    which is very useful when you're not exactly sure of what you're looking for.
+
+If you experience any errors while trying to install kickstart, run `:checkhealth` for more info.
+--]]
+
+-- Set <space> as the leader key
+-- See `:help mapleader`
+--  NOTE: Must happen before plugins are loaded (otherwise wrong leader will be used)
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
+
+-- Set to true if you have a Nerd Font installed and selected in the terminal
+vim.g.have_nerd_font = false
+
+vim.opt.termguicolors = true
+
+-- [[ Setting options ]]
+require 'options'
+
+-- [[ Basic Keymaps ]]
+require 'keymaps'
+
+-- [[ Compilation Hotkey ]]
+require 'terminal-hotkeys'
+
+-- [[ Basic Autocommands ]]
+require 'autocommands'
+
+-- [[ Install `lazy.nvim` plugin manager ]]
+require 'lazy-bootstrap'
+
+-- [[ Configure and install plugins ]]
+require 'lazy-plugins'
+
+-- The line beneath this is called `modeline`. See `:help modeline`
+-- vim: ts=2 sts=2 sw=2 et
---
+++ copy_dotfiles_to_this_repo.sh
@@ -0,0 +1,4 @@
+mkdir -p ./.config/
+cp -ri ~/.config/nvim/ ./.config/nvim/
+
+cp -i ~/.tmux.conf ./.tmux.conf
